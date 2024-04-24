44d78c59146d34ee064bf3bc5dece54237cf6a73
karmadice <krutarth.patel7@gmail.com>
2024-04-23 23:58:28 -0400

    Adding button Component

---
+++ src/Components/Button.vue
@@ -0,0 +1,31 @@
+<script setup>
+defineProps({
+    classes: {
+        type: String,
+        default: "inline-flex justify-center items-center whitespace-nowrap focus:outline-none transition-colors focus:ring duration-150 border cursor-pointer rounded border-gray-100 dark:border-slate-800 ring-gray-200 dark:ring-gray-500 bg-gray-100 text-black dark:bg-slate-800 dark:text-white hover:bg-gray-200 hover:dark:bg-slate-700 py-2 px-3 mr-3 last:mr-0 mb-3"
+    },
+    id: {
+        type: String,
+        default: null
+    },
+    label: {
+        type: [String, Number],
+        required: false,
+        default: null
+    },
+     icon: {
+        type: String,
+        required: false,
+        default: null
+     }
+})
+</script>
+
+<template>
+    <button
+    :class="classes"
+    :id="id"
+    >
+        <slot />
+    </button>
+</template>
--- src/Pages/Dashboard.vue
+++ src/Pages/Dashboard.vue
@@ -1,13 +1,14 @@
 <script setup>
 import LayoutAuthenticated from '../Layouts/LayoutAuthenticated.vue';
 import Image from '../Components/Image.vue';
+import Button from '../Components/Button.vue';
 </script>
 
 <template>
     <LayoutAuthenticated>
         <main>
             <h1>Hello, This is dashboard</h1>
-            <Image source="https://picsum.photos/200/300" alts="This is test" classes="ring-8"></Image>
+            <Button>This is Test Button</Button>
         </main>
     </LayoutAuthenticated>
 </template>
