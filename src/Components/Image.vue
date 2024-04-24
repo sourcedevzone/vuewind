9b53b1fb187ca6568f4525214113537852a81839
karmadice <krutarth.patel7@gmail.com>
2024-04-23 23:32:02 -0400

    Added Image Component

---
+++ src/Components/Image.vue
@@ -0,0 +1,25 @@
+<template>
+    <img :src="source" :alt="alts" :class="classes" :id="id"  />
+    <slot />
+</template>
+
+<script setup>
+defineProps({
+    source: {
+        type: String,
+        required: true
+    },
+    alts: {
+        type: String,
+        required: true
+    },
+    classes: {
+        type: String,
+        required: false
+    },
+    id: {
+        type: String,
+        required: false
+    },
+})
+</script>
